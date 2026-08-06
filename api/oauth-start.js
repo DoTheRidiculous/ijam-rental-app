0228f74856cb240f3d5224df9a8af603429cbf5c
DoTheRidiculous <122350367+DoTheRidiculous@users.noreply.github.com>
2026-08-05 21:21:36 -0500

    Create oauth-start.js

---
+++ api/oauth-start.js
@@ -0,0 +1,18 @@
+import { google } from "googleapis";
+
+export default function handler(req, res) {
+  const oauth2Client = new google.auth.OAuth2(
+    process.env.GOOGLE_CLIENT_ID,
+    process.env.GOOGLE_CLIENT_SECRET,
+    `https://${req.headers.host}/api/oauth-callback`
+  );
+
+  const url = oauth2Client.generateAuthUrl({
+    access_type: "offline",
+    prompt: "consent",
+    scope: ["https://www.googleapis.com/auth/drive"],
+  });
+
+  res.writeHead(302, { Location: url });
+  res.end();
+}
